290cad6e3aed04dea3c07d8e99876a29be74e91d
Kellwyn Sushanth <27.sushanth@gmail.com>
2022-11-09 22:24:57 +1300

    Add files via upload

---
+++ Data Ingestion with Kafka & Kafka Streaming/Practice Exercises/exercise6.5 Faust Stream Processors and Operations.py
@@ -0,0 +1,48 @@
+# Please complete the TODO items in the code
+
+from dataclasses import asdict, dataclass
+import json
+import random
+
+import faust
+
+
+@dataclass
+class ClickEvent(faust.Record):
+    email: str
+    timestamp: str
+    uri: str
+    number: int
+    score: int = 0
+
+
+#
+# TODO: Define a scoring function for incoming ClickEvents.
+#       It doens't matter _how_ you score the incoming records, just perform
+#       some modification of the `ClickEvent.score` field and return the value
+#
+def add_score(ClickEvent):
+    ClickEvent.score = ClickEvent.score + 1000
+    return ClickEvent
+
+
+app = faust.App("exercise5", broker="kafka://localhost:9092")
+clickevents_topic = app.topic("com.udacity.streams.clickevents", value_type=ClickEvent)
+scored_topic = app.topic(
+    "com.udacity.streams.clickevents.scored",
+    key_type=str,
+    value_type=ClickEvent,
+)
+
+@app.agent(clickevents_topic)
+async def clickevent(clickevents):
+    clickevents.add_processor(add_score)
+    # TODO: Add the `add_score` processor to the incoming clickevents
+    #       See: https://faust.readthedocs.io/en/latest/reference/faust.streams.html?highlight=add_processor#faust.streams.Stream.add_processor
+    #
+
+    async for ce in clickevents:
+        await scored_topic.send(key=ce.uri, value=ce)
+
+if __name__ == "__main__":
+    app.main()
